efd2ae41b9f53c3921ad6a785d2ce1ff6b0f9372
Phuc Nguyen <nguyenhuynhhongphuc.work@gmail.com>
2022-05-31 14:49:09 +0700

    add highlight component

--- src/components/Highlight/index.js
+++ src/components/Highlight/index.js
@@ -1,7 +1,47 @@
+import { Card, CardContent, Grid, Typography } from "@mui/material";
 import React from "react";
 
 const Highlight = () => {
-  return <div>Highlight</div>;
+  return (
+    <Grid container spacing={3}>
+      <Grid item sm={4} xs={12}>
+        <Card>
+          <CardContent>
+            <Typography component="p" variant="body2">
+              Số ca mắc
+            </Typography>
+            <Typography component="span" variant="body2">
+              3000
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+      <Grid item sm={4} xs={12}>
+        <Card>
+          <CardContent>
+            <Typography component="p" variant="body2">
+              Số ca khỏi
+            </Typography>
+            <Typography component="span" variant="body2">
+              3000
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+      <Grid item sm={4} xs={12}>
+        <Card>
+          <CardContent>
+            <Typography component="p" variant="body2">
+              Số ca tử vong
+            </Typography>
+            <Typography component="span" variant="body2">
+              3000
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Highlight;
