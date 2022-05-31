633d26b5610d87e61120b507acbb5efc5f7f72da
Phuc Nguyen <nguyenhuynhhongphuc.work@gmail.com>
2022-05-31 14:07:01 +0700

    finish country selector

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { getCountries } from "./apis";
+import CountrySelector from "./components/CountrySelector";
+import Highlight from "./components/Highlight";
+import Summary from "./components/Summary";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    getCountries().then((res) => {
+      setCountries(res.data);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CountrySelector countries={countries} />
+      <Highlight />
+      <Summary />
+    </>
   );
 }
 
---
+++ src/components/Highlight/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Highlight = () => {
+  return <div>Highlight</div>;
+};
+
+export default Highlight;
